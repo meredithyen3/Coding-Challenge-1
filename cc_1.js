26d51716aea5e0fe8dff173a36351421ea2775b7
meredithyen3 <meredithyen@usf.edu>
2025-01-26 11:12:45 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+// Task 1: Variable Declaration
+let employeeName = "Meredith";
+console.log(employeeName);
+
+const employeeID = 845503;
+console.log(employeeID);
+
+var isActive = true;
+console.log(isActive);
